ef922c17b67975266982d9027dff8c6c3f29064c
Pete Franco <petethe4th@live.com>
2023-04-25 14:10:12 -0500

    layout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components'
+import {FaSearch} from "react-icons/fa"
+import {CgProfile} from "react-icons/cg"
+import { useStateProvider } from '../utils/StateProvider'
+
+export default function Navbar() {
+    const [{userInfo}] = useStateProvider()
+  return (
+    <Container>
+        <div className="search__bar">
+            <FaSearch />
+            <input type='text' placeholder='Artits, songs, or podcasts' />
+        </div>
+        <div className="avatar">
+            <a href='#'>
+                <CgProfile />
+                <span>{userInfo?.userName}</span>
+            </a>
+        </div>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 2rem;
+    height: 15vh;
+    position: sticky;
+    top: 0;
+    transition: 0.3s ease-in-out;
+    background-color: none;
+    .search__bar {
+        background-color: white;
+        width: 30%;
+        padding: 0.4rem 1rem;
+        border-radius: 2rem;
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+        input {
+            border: none;
+            height: 2rem;
+            width: 100%;
+            &:focus {
+                outline: none;
+            }
+        }
+    }
+`
